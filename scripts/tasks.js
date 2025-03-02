6853b70a345fcfccea8bda4734342ff6b115dd75
tsgm1257 <neaz1257@gmail.com>
2025-03-02 03:25:13 -0500

    Added multiple background color

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -3,6 +3,34 @@ const activityLog = document.getElementById("activity-log");
 const clearButton = document.getElementById("clear-btn");
 const currentDay = document.getElementById("current-day");
 const currentDate = document.getElementById("current-date");
+const themeButton = document.getElementById("theme-btn");
+const body = document.body;
+const bgColors = [
+  "bg-red-500",
+  "bg-blue-500",
+  "bg-green-500",
+  "bg-yellow-500",
+  "bg-purple-500",
+  "bg-pink-500",
+  "bg-orange-500",
+  "bg-cyan-500",
+  "bg-teal-500",
+  "bg-lime-500",
+  "bg-indigo-500",
+  "bg-gray-500",
+  "bg-emerald-500",
+  "bg-sky-500",
+  "bg-rose-500",
+  "bg-fuchsia-500",
+  "bg-violet-500",
+];
+
+themeButton.addEventListener("click", function () {
+  body.className = "";
+
+  const randomColor = Math.floor(Math.random() * bgColors.length);
+  body.classList.add(bgColors[randomColor]);
+});
 
 const now = new Date();
 const options = {
